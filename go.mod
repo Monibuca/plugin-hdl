1fa44e1287317e512ea62b014ffaefa94b67726f
langhuihui <178529795@qq.com>
2020-08-13 20:16:24 +0800

    增加metadata的tag写入

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.13
 require (
 	github.com/Monibuca/engine/v2 v2.1.1
 	github.com/logrusorgru/aurora v0.0.0-20200102142835-e9ef32dff381
+	github.com/zhangpeihao/goamf v0.0.0-20140409082417-3ff2c19514a8
 )
--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package hdl
 
 import (
+	"bytes"
+	"encoding/binary"
 	"log"
 	"net/http"
 	"strings"
@@ -8,6 +10,7 @@ import (
 	. "github.com/Monibuca/engine/v2"
 	"github.com/Monibuca/engine/v2/avformat"
 	. "github.com/logrusorgru/aurora"
+	"github.com/zhangpeihao/goamf"
 )
 
 var config = new(ListenerConfig)
@@ -42,6 +45,32 @@ func HDLHandler(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Transfer-Encoding", "chunked")
 		w.Header().Set("Content-Type", "video/x-flv")
 		w.Write(avformat.FLVHeader)
+		var metadata avformat.SendPacket
+		metadata.AVPacket = new(avformat.AVPacket)
+		metadata.Type = avformat.FLV_TAG_TYPE_SCRIPT
+		var buffer bytes.Buffer
+		amf.WriteString(&buffer, "onMetaData")
+		WriteEcmaArray(&buffer, amf.Object{
+			"MetaDataCreator": "monibuca",
+			"hasVideo":        true,
+			"hasAudio":        true,
+			"hasMatadata":     true,
+			"canSeekToEnd":    false,
+			"duration":        0,
+			"hasKeyFrames":    0,
+			"videocodecid":    int(s.VideoInfo.CodecID),
+			"framerate":       0,
+			"videodatarate":   0,
+			"audiocodecid":    int(s.AudioInfo.SoundFormat),
+			"filesize":        0,
+			"width":           s.VideoInfo.SPSInfo.Width,
+			"height":          s.VideoInfo.SPSInfo.Height,
+			"audiosamplerate": s.AudioInfo.SoundRate,
+			"audiosamplesize": int(s.AudioInfo.SoundSize),
+			"stereo":          s.AudioInfo.SoundType == 1,
+		})
+		metadata.Payload = buffer.Bytes()
+		avformat.WriteFLVTag(w, &metadata)
 		p := Subscriber{
 			Sign: sign,
 			OnData: func(packet *avformat.SendPacket) error {
@@ -55,4 +84,31 @@ func HDLHandler(w http.ResponseWriter, r *http.Request) {
 	} else {
 		w.WriteHeader(404)
 	}
+}
+func WriteEcmaArray(w amf.Writer, o amf.Object) (n int, err error) {
+	n, err = amf.WriteMarker(w, amf.AMF0_ECMA_ARRAY_MARKER)
+	if err != nil {
+		return
+	}
+	length := int32(len(o))
+	err = binary.Write(w, binary.BigEndian, &length)
+	if err != nil {
+		return
+	}
+	n += 4
+	m := 0
+	for name, value := range o {
+		m, err = amf.WriteObjectName(w, name)
+		if err != nil {
+			return
+		}
+		n += m
+		m, err = amf.WriteValue(w, value)
+		if err != nil {
+			return
+		}
+		n += m
+	}
+	m, err = amf.WriteObjectEndMarker(w)
+	return n + m, err
 }
